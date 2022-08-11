54fa79445f04685220b42d8e2b1bbfc0fef77f7e
Ehimwenma <ehimwenma.idemudia@outlook.com>
2022-08-11 09:14:42 -0700

    Finally resolve Merge-Head conflict... GIT is scary

---
+++ prompt.c
@@ -0,0 +1,21 @@
+#include "shell.h"
+
+/**
+ * prompt - Prompts for input
+ * @str: The pointer to store user input
+ * @mode: The mode of the shell
+ *
+ * Return: void
+ */
+size_t prompt(char **str, int *mode)
+{
+size_t len = 0;
+size_t status;
+
+if (!isatty(STDIN_FILENO))
+*mode = 0;
+else
+write(1, "$ ", 2);
+status = getline(str, &len, stdin);
+return (status);
+}
